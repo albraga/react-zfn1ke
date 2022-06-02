7bd888038ec9e3bcf4002d52031e9ddd31fd171f
albraga <alexandre_cgr@hotmail.com>
2022-06-02 15:40:18 -0300

    2 files modified

--- src/Lista.js
+++ src/Lista.js
@@ -11,14 +11,18 @@ const Lista = () => {
   const hchange = id => {
     setItems(items.map(item => (item.id === id ? {...item, checked: !item.checked} : item)))
   }
+  const hdel = id => {
+    setItems(items.filter(item => item.id !== id))
+  }
   return (
     <ListGroup>
       {items.map((item) => (
         <ListGroup.Item>
           <Stack direction="horizontal" gap={3}>
+            
             <div className="bg-light border "><input type="checkbox" checked={item.checked} onChange={()=>hchange(item.id)} /></div>
             <div className="bg-light border"><label onDoubleClick={() => hchange(item.id)}>{item.desc}</label></div>
-            <div className="bg-light border  ms-auto"><FaTrashAlt /></div>
+            <div className="bg-light border  ms-auto"><FaTrashAlt onClick={() => hdel(item.id)} role="button"/></div>
           </Stack>          
         </ListGroup.Item>
       ))}
